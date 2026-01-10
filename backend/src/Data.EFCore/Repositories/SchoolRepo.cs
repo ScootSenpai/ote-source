02c8fb82397ace11297b14d03bc138fa4fa5b129
Michael Mark <michael.mark@oit.edu>
2026-01-09 21:27:22 -0800

    Changed repos to use entities

--- backend/src/Data.EFCore/Repositories/SchoolRepo.cs
+++ backend/src/Data.EFCore/Repositories/SchoolRepo.cs
@@ -1,10 +1,9 @@
 using OTE.Data.EFCore.Contexts;
-using OTE.Data.EFCore.Dtos;
 using OTE.Data.EFCore.Entities;
 
 namespace OTE.Data.EFCore.Repositories;
 
 /// <summary>`SchoolEntity` implementation of `AbstractRepo`.</summary>
 /// <param name="context">The `OteContext` the repository uses.</summary>
-public class SchoolRepo(OteContext context) : AbstractRepo<SchoolEntity, SchoolDto>(context)
+public class SchoolRepo(OteContext context) : AbstractRepo<SchoolEntity>(context)
 {}
